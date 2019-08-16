2177e06a4da9612847b18e1b39fa7cb34a825e33
Hardelele <i.lapshina@mail.ru>
2019-08-16 21:04:53 +0300

    TestController.java hb renamed from HelloWorldController.java

--- src/main/java/org/perfectstorms/demo/controllers/TestController.java
+++ src/main/java/org/perfectstorms/demo/controllers/TestController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
-    @GetMapping("/hellow")
-    public String hellow(){
-        return "Hello world!!!";
+public class TestController {
+    @GetMapping("/MariaDB")
+    public String test(){
+        return "";
     }
 }
