2b06e9b4fdf789e1b198814e05fe0c1b95f2fc4e
Hardelele <i.lapshina@mail.ru>
2019-08-17 14:49:56 +0300

    Edit TestRepository.java

--- src/main/java/org/perfectstorms/demo/repository/TestRepository.java
+++ src/main/java/org/perfectstorms/demo/repository/TestRepository.java
@@ -1,8 +1,15 @@
 package org.perfectstorms.demo.repository;
 
 import org.perfectstorms.demo.entities.TestEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TestRepository extends CrudRepository<TestEntity, Integer> {
-    
+
+    @Query (value = "SELECT * FROM test mytable WHERE mytable.id = :id ", nativeQuery = true)
+    TestEntity getOneById(@Param("id") Long id);
+
 }
